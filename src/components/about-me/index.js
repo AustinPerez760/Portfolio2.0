690cea70cf731e0320f9d7fdbac4c276fec37832
Austin Perez <austin.perez760@gmail.com>
2025-01-23 13:29:36 -0600

    fixed typos

--- src/components/about-me/index.js
+++ src/components/about-me/index.js
@@ -1,19 +1,18 @@
-import React from "react";
-import "./style.css";
-import mediaPic from "../../assets/images/social-pics2.jpg";
+import React from 'react';
+import './style.css';
+import mediaPic from '../../assets/images/social-pics2.jpg';
 
 export default function aboutMe() {
   return (
-    <section className="sec2">
-      <div className="main-container">
-        <div className="about-wrapper">
-          <div className="about-me">
+    <section className='sec2'>
+      <div className='main-container'>
+        <div className='about-wrapper'>
+          <div className='about-me'>
             <h4>ABOUT ME</h4>
             <p>
-              A team-oriented self-motivated developer eager to lead through
-              innovation and collaboration in order to produce feature-rich,
-              dynamic web applications to share with friends, peers, and the
-              world wide web
+              A team-oriented developer eager to lead through innovation and
+              collaboration in order to produce feature-rich, dynamic web
+              applications to share with friends, peers, and the world wide web
             </p>
 
             <p>
@@ -29,47 +28,58 @@ export default function aboutMe() {
               Full stack devloper proficient in a wide variety of front end and
               back end web languages, technologies, and frameworks:
               <a
-                target="_blank"
-                href="./Resources/Austin Perez Dev Resume-4.pdf"
+                target='_blank'
+                href='./Resources/Austin Perez Dev Resume-4.pdf'
                 download
               >
                 Download My Resume
               </a>
             </p>
 
-            <div id="skills">
+            <div id='skills'>
               <ul>
                 <li>HTML</li>
                 <li>CSS</li>
                 <li>JavaScript</li>
                 <li>JQUERY</li>
                 <li>React</li>
+                <li>Vue.js</li>
+                <li>Typescript</li>
+                <li>LIT</li>
+                <li>Python</li>
+                <li>Mdui3</li>
+                <li>Vite</li>
+                <li>Vitest</li>
               </ul>
               <ul>
-                <li>Node.JS</li>
+                <li>Node.js</li>
                 <li>Express</li>
                 <li>MySQL</li>
                 <li>MongoDB</li>
                 <li>Sequelize</li>
+                <li>DynamoDB</li>
+                <li>Figma</li>
+                <li>Jest</li>
+                <li>Storybook</li>
               </ul>
             </div>
           </div>
 
-          <div className="social">
+          <div className='social'>
             <img
-              id="media-pic"
+              id='media-pic'
               src={mediaPic}
-              alt="user linkedin and github profiles"
+              alt='user linkedin and github profiles'
             />
             <h3>Check me out on LinkedIn and GitHub</h3>
             <a
-              targt="_blank"
-              href="https://www.linkedin.com/in/austin-perez-3b1899205"
+              targt='_blank'
+              href='https://www.linkedin.com/in/austin-perez-3b1899205'
             >
               LinkedIn
             </a>
             <br />
-            <a targt="_blank" href="https://github.com/AustinPerez760">
+            <a targt='_blank' href='https://github.com/AustinPerez760'>
               GitHub
             </a>
           </div>
--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
-import austinPic from "../../assets/images/Austin3.jpg";
+import React, { useState } from 'react';
+import austinPic from '../../assets/images/Austin3.jpg';
 // import abyssCss from "../../assets/css/abyss.css";
-import "./style.css";
+import './style.css';
 
 // function useForceUpdate() {
 //   const [value, setValue] = useState(0);
@@ -35,63 +35,62 @@ export default function Jumbotron() {
   //     localStorage.setItem("theme", mode);
   //   }
   return (
-    <section className="sec1">
-      <div className="main-container">
-        <div className="head-wrapper">
-          <h1>Hi, I'm Samuel "Austin" Perez</h1>
+    <section className='sec1'>
+      <div className='main-container'>
+        <div className='head-wrapper'>
+          <h1>Hi, I'm Sam Perez</h1>
         </div>
 
-        <div className="intro-wrapper">
-          <div className="nav-wrapper">
-            <div className="dots-wrapper">
-              <div id="dot-1" className="browser-dot"></div>
-              <div id="dot-2" className="browser-dot"></div>
-              <div id="dot-3" className="browser-dot"></div>
+        <div className='intro-wrapper'>
+          <div className='nav-wrapper'>
+            <div className='dots-wrapper'>
+              <div id='dot-1' className='browser-dot'></div>
+              <div id='dot-2' className='browser-dot'></div>
+              <div id='dot-3' className='browser-dot'></div>
             </div>
           </div>
 
-          <div className="left-column">
-            <img id="profile-pic" src={austinPic} alt="self portrait" />
-            <h5 className="theme-title">Customize Theme</h5>
-            <div id="theme-options-wrapper">
+          <div className='left-column'>
+            <img id='profile-pic' src={austinPic} alt='self portrait' />
+            <h5 className='theme-title'>Customize Theme</h5>
+            <div id='theme-options-wrapper'>
               <div
-                data-mode="light"
-                id="light-mode"
-                className="theme-dots"
+                data-mode='light'
+                id='light-mode'
+                className='theme-dots'
               ></div>
-              <div data-mode="dark" id="dark-mode" className="theme-dots"></div>
+              <div data-mode='dark' id='dark-mode' className='theme-dots'></div>
               <div
-                data-mode="abyss"
-                id="abyss-mode"
-                className="theme-dots"
+                data-mode='abyss'
+                id='abyss-mode'
+                className='theme-dots'
                 // onClick={setTheme}
               ></div>
               <div
-                data-mode="elysium"
-                id="elysium-mode"
-                className="theme-dots"
+                data-mode='elysium'
+                id='elysium-mode'
+                className='theme-dots'
               ></div>
             </div>
 
-            <p id="setting-note">
+            <p id='setting-note'>
               *Custom settings will be saved for <br />
               your next visit
             </p>
           </div>
 
-          <div className="right-column">
-            <div id="preview-shadow">
-              <div id="preview">
-                <div id="corner-tl" className="corner"></div>
-                <div id="corner-tr" className="corner"></div>
+          <div className='right-column'>
+            <div id='preview-shadow'>
+              <div id='preview'>
+                <div id='corner-tl' className='corner'></div>
+                <div id='corner-tr' className='corner'></div>
                 <h3>What Is My Deal?</h3>
                 <p>
-                  Full stack web developer certified by the University of San
-                  Diego who is ready to show the web what my skills have to
-                  offer
+                  Full-stack web developer certified by the University of San
+                  Diego eager to make on impact on all my web endeours.
                 </p>
-                <div id="corner-br" className="corner"></div>
-                <div id="corner-bl" className="corner"></div>
+                <div id='corner-br' className='corner'></div>
+                <div id='corner-bl' className='corner'></div>
               </div>
             </div>
           </div>
